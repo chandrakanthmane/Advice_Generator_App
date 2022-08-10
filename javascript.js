1141bb3a4412bb236ad95b4788bb3015c88f4ec9
chandrakanthmane <chandrakanthmane84@gmail.com>
2022-08-10 19:06:05 +0530

    second commit

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 
 async function exampleFetch() {
+    
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
 
     document.getElementById("adviceId").innerHTML= data.slip.id;
     document.getElementById("advice").innerHTML= data.slip.advice;
+    // document.getElementById('dice').className = 'classname';
 }
 
 exampleFetch()
